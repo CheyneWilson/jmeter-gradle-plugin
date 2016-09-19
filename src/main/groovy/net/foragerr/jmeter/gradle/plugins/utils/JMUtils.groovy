5f2633a732c9fc940893d982c56a97f351bb4f48
Cheyne Wilson <cheyne.wilson@airnz.co.nz>
2016-09-19 15:57:27 +1200

    Refactor to reduce code branches in if/else block. Add warning when search dir has no jmeter test files.

--- src/main/groovy/net/foragerr/jmeter/gradle/plugins/utils/JMUtils.groovy
+++ src/main/groovy/net/foragerr/jmeter/gradle/plugins/utils/JMUtils.groovy
@@ -4,6 +4,7 @@ import groovy.util.logging.Slf4j
 import org.apache.tools.ant.DirectoryScanner
 import org.gradle.api.GradleException
 import org.gradle.api.Project
+import org.gradle.api.logging.Logging
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -14,21 +15,24 @@ import java.text.SimpleDateFormat
 @Slf4j
 class JMUtils {
 
+    static final LOG = Logging.getLogger(getClass())
+
     static List<File> getListOfTestFiles(Project project){
         List<File> testFiles = new ArrayList<File>();
-        if (project.jmeter.jmTestFiles != null) {
-            project.jmeter.jmTestFiles.each { File file ->
+        if (project.jmeter.testFiles != null) {
+            project.jmeter.testFiles.each { File file ->
                 if (file.exists() && file.isFile()) {
                     testFiles.add(file);
                 } else {
-                    throw new GradleException("Test file " + file.getCanonicalPath() + " does not exists");
+                    throw new GradleException("Test file ${file.getCanonicalPath()} does not exists");
                 }
             }
         } else {
             String[] excludes = project.jmeter.excludes == null ?  [] as String[] : project.jmeter.excludes as String[];
             String[] includes = project.jmeter.includes == null ? ["**/*.jmx"] as String[] : project.jmeter.includes as String[];
-            log.info("includes: " + includes)
-            log.info("excludes: " + excludes)
+
+            log.info("includes: ${includes}")
+            log.info("excludes: ${excludes}")
             testFiles.addAll(JMUtils.scanDir(project, includes, excludes, project.jmeter.testFileDir));
             log.info(testFiles.size() + " test files found in folder scan")
         }
@@ -40,8 +44,8 @@ class JMUtils {
         File propsInSrcDir = new File(project.jmeter.testFileDir,"jmeter.properties");
 
         //1. Is jmeterPropertyFile defined?
-        if (project.jmeter.jmPropertyFile != null) {
-            return project.jmeter.jmPropertyFile;
+        if (project.jmeter.propFile != null) {
+            return project.jmeter.propFile;
         }
 
         //2. Does jmeter.properties exist in $srcDir/test/jmeter
@@ -57,22 +61,20 @@ class JMUtils {
     }
 
 	static File getResultFile(File testFile, Project project) {
-		DateFormat fmt = new SimpleDateFormat("yyyyMMdd-HHmm");
-		if (project.jmeter.resultFilenameTimestamp==null)
-			return new File(project.jmeter.reportDir, testFile.getName() + "-" + fmt.format(new Date()) + ".xml");
-
-		//if resultFilenameTimestamp is "none" do not use a timestamp in filename
-		if (project.jmeter.resultFilenameTimestamp.equals("none"))
-			return new File(project.jmeter.reportDir, testFile.getName() + ".xml");
-
-		//else if resultFilenameTimestamp is "useSaveServiceFormat" use saveservice.format
+        if (testFile == null){
+            return null
+        }
+        DateFormat defaultFmt = new SimpleDateFormat("yyyyMMdd-HHmm");
+		//if resultFilenameTimestamp is "useSaveServiceFormat" use saveservice.format
 		if (project.jmeter.resultFilenameTimestamp.equals("useSaveServiceFormat")){
 			String saveServiceFormat =  System.getProperty("jmeter.save.saveservice.timestamp_format");
-			if (saveServiceFormat.equals("none")) return new File(project.jmeter.reportDir, testFile.getName() + ".xml");
+			if (saveServiceFormat.equals("none")) {
+                return new File(project.jmeter.reportDir, "${testFile.getName()}.xml");
+            }
 			try
 			{
-				fmt = new SimpleDateFormat(saveServiceFormat);
-				return new File(project.jmeter.reportDir, testFile.getName() + "-" + fmt.format(new Date()) + ".xml");
+                DateFormat saveFormat = new SimpleDateFormat(saveServiceFormat);
+				return new File(project.jmeter.reportDir, "${testFile.getName()}-${saveFormat.format(new Date())}.xml");
 			}
 			catch (Exception e)
 			{
@@ -80,21 +82,33 @@ class JMUtils {
 				log.warn("jmeter.save.saveservice.timestamp_format Not defined, using default timestamp format");
 			}
 		}
-        //for all other unhandled conditions fallback to default:
-        fmt = new SimpleDateFormat("yyyyMMdd-HHmm");
-        return new File(project.jmeter.reportDir, testFile.getName() + "-" + fmt.format(new Date()) + ".xml");
+
+        //if resultFilenameTimestamp is "none" do not use a timestamp in filename
+        else if (project.jmeter.resultFilenameTimestamp.equals("none")) {
+            return new File(project.jmeter.reportDir, "${testFile.getName()}.xml");
+        }
+
+        else if (project.jmeter.resultFilenameTimestamp==null) {
+            return new File(project.jmeter.reportDir, "${testFile.getName()}-${defaultFmt.format(new Date())}.xml");
+        }
+
+        return new File(project.jmeter.reportDir, "${testFile.getName()}-${defaultFmt.format(new Date())}.xml");
     }
 
 	
     static  List<File> scanDir(Project project, String[] includes, String[] excludes, File baseDir) {
         List<File> scanResults = new ArrayList<File>()
-        DirectoryScanner scanner = new DirectoryScanner()
-        scanner.setBasedir(baseDir)
-        scanner.setIncludes(includes)
-        scanner.setExcludes(excludes)
-        scanner.scan()
-        for (String result : scanner.getIncludedFiles()) {
-            scanResults.add(new File(scanner.getBasedir(), result))
+        if (baseDir.exists()) {
+            DirectoryScanner scanner = new DirectoryScanner()
+            scanner.setBasedir(baseDir)
+            scanner.setIncludes(includes)
+            scanner.setExcludes(excludes)
+            scanner.scan()
+            for (String result : scanner.getIncludedFiles()) {
+                scanResults.add(new File(scanner.getBasedir(), result))
+            }
+        } else {
+            LOG.warn("Attempted to load Jmeter files from {}, but this directory does not exist.", baseDir)
         }
         return scanResults;
     }
